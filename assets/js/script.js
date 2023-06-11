fe8dbfa601ee775545f4fe8f947534ca8a690999
Gabriella Köhler <gabriella.kohler91@gmail.com>
2023-06-11 23:10:52 +0000

    added javascript to display the questions and answers in the elements

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,122 +1,145 @@
 const quizQuestions = [
     {
         question: "What is the capital of the alliance?",
-        answers: [
-            { text: "Ironforge", correct: false},
-            { text: "Stormwind", correct: true},
-            { text: "Orgrimmar", correct: false},
-            { text: "Darnassus", correct: false},
-        ]
-    },
+        answers: 
+            {choice1:"Ironforge",
+            choice2:"Stormwind",
+            choice3:"Orgrimmar",
+            choice4:"Darnassus"}
+        
+       , correctAnswer: 1
+     } /* ,
 
     {
         question: "Who is the leader of the horde?",
         answers: [
-            { text: "Sylvanas Windrunner", correct: false},
-            { text: "Your mom", correct: false},
-            { text: "Your dad", correct: false},
-            { text: "Thrall", correct: true},
+            { choice1: "Sylvanas Windrunner"},
+            { choice2: "Your mom"},
+            { text: "Your dad"},
+            { text: "Thrall"},
         ]
+        , correctAnswer: 3
     },
 
     {
         question: "How many bosses does Deadmines have?",
         answers: [
-            { text: "7", correct: true},
-            { text: "6", correct: false},
-            { text: "9", correct: false},
-            { text: "8", correct: false},
+            { text: "7"},
+            { text: "6"},
+            { text: "9"},
+            { text: "8"},
         ]
+        , correctAnswer: 0
     },
 
     {
         question: "Which bosses drops the bindings for Thunderfury?",
         answers: [
-            { text: "Lucifron and Shazzrah", correct: false},
-            { text: "Baron Geddon and Ragnaros", correct: false},
-            { text: "Garr and Baron Geddon", correct: true},
-            { text: "Sulfuron and Garr", correct: false},
+            { text: "Lucifron and Shazzrah"},
+            { text: "Baron Geddon and Ragnaros"},
+            { text: "Garr and Baron Geddon"},
+            { text: "Sulfuron and Garr"},
         ]
+        , correctAnswer: 2
     },
 
     {
         question: "What level does druids get their bearform at?",
         answers: [
-            { text: "12", correct: false},
-            { text: "11", correct: false},
-            { text: "14", correct: false},
-            { text: "10", correct: true},
+            { text: "12"},
+            { text: "11"},
+            { text: "14"},
+            { text: "10"},
         ]
+        , correctAnswer: 3
     },
 
     {
         question: "What is the racial mount for trolls?",
         answers: [
-            { text: "Raptor", correct: true},
-            { text: "Kodo", correct: false},
-            { text: "Wolf", correct: false},
-            { text: "Skeletal Horse", correct: false},
+            { text: "Raptor"},
+            { text: "Kodo"},
+            { text: "Wolf"},
+            { text: "Skeletal Horse"},
         ]
+        , correctAnswer: 0
     },
 
     {
         question: "Who is the last boss of AQ20?",
         answers: [
-            { text: "C'thun", correct: false},
-            { text: "Ossirian the Unscarred", correct: true},
-            { text: "Moam", correct: false},
-            { text: "Ayamiss the Hunter", correct: false},
+            { text: "C'thun"},
+            { text: "Ossirian the Unscarred"},
+            { text: "Moam"},
+            { text: "Ayamiss the Hunter"},
         ]
+        , correctAnswer: 1
     },
 
     {
         question: "How much gold does paladins have to pay at the start of their epic mount quest?",
         answers: [
-            { text: "300", correct: false},
-            { text: "150", correct: true},
-            { text: "100", correct: false},
-            { text: "500", correct: false},
+            { text: "300"},
+            { text: "150"},
+            { text: "100"},
+            { text: "500"},
         ]
+        , correctAnswer: 1
     },
 
     {
         question: "Which class can't gnomes play as?",
         answers: [
-            { text: "Rogue", correct: false},
-            { text: "Warrior", correct: false},
-            { text: "Hunter", correct: true},
-            { text: "Mage", correct: false},
+            { text: "Rogue"},
+            { text: "Warrior"},
+            { text: "Hunter"},
+            { text: "Mage"},
         ]
+        , correctAnswer: 2
     },
 
     {
         question: "Which horde race can be a druid?",
         answers: [
-            { text: "Troll", correct: false},
-            { text: "Orc", correct: false},
-            { text: "Undead", correct: false},
-            { text: "Tauren", correct: true},
+            { text: "Troll"},
+            { text: "Orc"},
+            { text: "Undead"},
+            { text: "Tauren"},
         ]
-    },
+        , correctAnswer: 3
+    } */
 
-];
-
-const questionElement = document.getElementById("question");
+]; 
 const answerButton = document.getElementById("answer-button");
 const nextButton = document.getElementById("next-btn");
 
 let currentQuestionIndex = 0;
 let score = 0;
 
+/* when starting quiz this function sets everything to 0 */
 function startQuiz(){
     currentQuestionIndex = 0;
     score = 0;
     nextButton.innerHTML = "Next";
-    showQuestion();
+    showQuestion(quizQuestions);
 }
 
-function showQuestion(){
-    let currentQuestion = questions[currentQuestionIndex];
+function showQuestion(q) {
+    let questionElement = document.getElementById("question");
+    questionElement.textContent = q[0].question;
+    
+    let choice1Element = document.getElementById("btn1");
+    let choice2Element = document.getElementById("btn2"); 
+    let choice3Element = document.getElementById("btn3"); 
+    let choice4Element = document.getElementById("btn4");  
+    choice1Element.textContent = q[0].answers.choice1;
+    choice2Element.textContent = q[0].answers.choice2;
+    choice3Element.textContent = q[0].answers.choice3;
+    choice4Element.textContent = q[0].answers.choice4;
+}
+
+/* function showQuestion(){
+    let currentQuestion = quizQuestions[currentQuestionIndex];
     let questionNo = currentQuestionIndex + 1;
     questionElement.innerHTML = questionNo + ". " + currentQuestion.question;
 
@@ -127,5 +150,6 @@ function showQuestion(){
         answerButton.appendChild(button)
     });
 }
+*/
 
 startQuiz();
